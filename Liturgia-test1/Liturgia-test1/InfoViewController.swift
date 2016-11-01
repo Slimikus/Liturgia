653f28c19064793ed7bef4d934ff123ba1a7456b
Slimikus <slimikus@gmail.com>
2016-11-01 12:58:35 +0300

    Создание проекта

---
+++ Liturgia-test1/Liturgia-test1/InfoViewController.swift
@@ -0,0 +1,40 @@
+//
+//  InfoViewController.swift
+//  Liturgia-test1
+//
+//  Created by Admin on 01.11.16.
+//  Copyright © 2016 Slimikus. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    @IBOutlet weak var textLabel: UILabel!
+    @IBAction func pushButton(_ sender: UIButton) {
+        print("Вывод текста в консоль")
+        textLabel.textColor = UIColor.green
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
